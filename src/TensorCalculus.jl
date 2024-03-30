367f88d16b67a22e7c70090dd6be4dde442e17fc
Christian Garcia <rojolocco@gmail.com>
2024-03-29 21:39:47 -0500

    Create abastrat type MainTensor

--- src/TensorCalculus.jl
+++ src/TensorCalculus.jl
@@ -1,5 +1,32 @@
 module TensorCalculus
 
-# Write your package code here.
+#----------------------------------------------------------------
+# Include exports from other files
+include("exports.jl")
+export MainTensor
+
+
+#----------------------------------------------------------------
+# Abstract types
+abstract type MainTensor{T<:AbstractFloat,order,dim} <: AbstractArray{T,order} end
+
+
+#----------------------------------------------------------------
+# Structs
+#create a struct called Tensor based on MainTensor with inner constructor
+
+
+
+#----------------------------------------------------------------
+# Include other julia files
+
+
+# Create Tensors
+include("create_tensors/zeros.jl")
+# include("create_tensors/ones.jl")
+# include("create_tensors/rand.jl")
+# include("create_tensors/eye.jl")
+# include("create_tensors/diag.jl")
+# include("create_tensors/copy.jl")
 
 end
